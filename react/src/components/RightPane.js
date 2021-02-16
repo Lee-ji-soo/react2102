741a7f75edb349ac7d3f1aee80a5545fe46f47b8
Lee-ji-soo <nbsp3@naver.com>
2021-02-16 17:52:27 +0900

    immer setting

---
+++ react/src/components/RightPane.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Receives from './Receives';
+
+const RightPane = () => {
+  return(
+    <div className="panes">
+      <Receives/>
+    </div>
+  )
+};
+
+export default RightPane;
