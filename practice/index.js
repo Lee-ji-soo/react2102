8ac97c13c39be1f789ca775883bd28d5af1a5741
Lee-ji-soo <nbsp3@naver.com>
2021-02-17 23:36:21 +0900

    구구단 validation

--- practice/index.js
+++ practice/index.js
@@ -1,16 +1,29 @@
-const $inputNumberForm = document.querySelector("#inputNumber_form");
-const $inputNumber = document.querySelector("#inputNumber");
+const $inputForm = document.querySelector("#inputNumber_form");
+const $input = document.querySelector("#inputNumber");
+
+const isValid = value => {
+  if ((typeof value !== 'number') || isNaN(value)) {
+    alert('숫자를 입력해 주세요.')
+  } else if (value < 2 || value > 9) {
+    alert('2~9 구구단 입니다.')
+  } else return true;
+}
 
 const onGugudan = value => {
   for (i = 1; i < 10; i++) {
     console.log(`${value} x ${i} = ${value * i}`)
-  };
+  }
 }
 
 const handleSubmit = (e) => {
   e.preventDefault();
-  const value = $inputNumber.value;
+  if ($input.value === ('' || undefined || null)) {
+    alert('값을 입력하세요');
+    return;
+  }
+  const value = parseInt($input.value, 10);
+  if (!isValid(value)) return;
   onGugudan(value);
 };
 
-$inputNumberForm.addEventListener("submit", handleSubmit);
+$inputForm.addEventListener("submit", handleSubmit);
