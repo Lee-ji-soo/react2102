fef0fa8dce9b2270a6be1423d90f62c5da283767
Lee-ji-soo <nbsp3@naver.com>
2021-02-20 01:31:54 +0900

    add input validation

--- practice/index.js
+++ practice/index.js
@@ -1,29 +1,43 @@
-const $inputForm = document.querySelector("#inputNumber_form");
-const $input = document.querySelector("#inputNumber");
-
-const isValid = value => {
-  if ((typeof value !== 'number') || isNaN(value)) {
-    alert('숫자를 입력해 주세요.')
-  } else if (value < 2 || value > 9) {
-    alert('2~9 구구단 입니다.')
-  } else return true;
-}
+const $inputForm = document.querySelector(".input_gugudan_form");
+const $input = document.querySelector(".input_gugudan");
 
 const onGugudan = value => {
-  for (i = 1; i < 10; i++) {
-    console.log(`${value} x ${i} = ${value * i}`)
+  for (num = 1; num < 10; num++) {
+    console.log(`${value} x ${num} = ${value * num}`)
   }
+}
+
+const refreshInput = () => {
+  $input.value = "";
+  $input.focus();
+}
+
+const isNum = value => {
+  String(value).replace(/^\s+|\s+$/g, "");
+  const reg = /^[0-9]*$/;
+  return reg.test(value);
+}
+
+const isValid = value => {
+  if ((value === "") || (value === null) || (value === undefined)) {
+    alert('값을 입력하세요');
+  } else if (isNum(value) && ((value < 2) || (value > 9))) {
+    alert('2~9 사이의 숫자만 입력하세요');
+  } else if (!isNum(value)) {
+    alert('숫자만 입력하세요');
+  } else return true;
+  refreshInput();
+  return;
 }
 
 const handleSubmit = (e) => {
   e.preventDefault();
-  if ($input.value === ('' || undefined || null)) {
-    alert('값을 입력하세요');
-    return;
+  const value = $input.value;
+  if (isValid(value)) {
+    onGugudan(value);
   }
-  const value = parseInt($input.value, 10);
-  if (!isValid(value)) return;
-  onGugudan(value);
+  refreshInput();
+  return;
 };
 
 $inputForm.addEventListener("submit", handleSubmit);
