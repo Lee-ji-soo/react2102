427bb98d90448d53f6f0850c7b5dadcb1b9561c2
Lee-ji-soo <nbsp3@naver.com>
2021-02-21 14:04:14 +0900

    add vendingMachine function

--- practice/vendingMachine.js
+++ practice/vendingMachine.js
@@ -1,7 +1,16 @@
 const $selects = document.querySelector("#selects");
 const $entryForm = document.querySelector("#entry_form");
-const $entry = $entryForm.querySelector("input")
-const $amount = document.querySelector("#amount");
+const $entry = $entryForm.querySelector("#entry")
+const $exit = document.querySelector("#exit");
+const $paid = document.querySelector("#paid");
+let paid = 0;
+let selectedArr = [];
+let exithtml = "";
+
+const refreshInput = () => {
+  $entry.value = "";
+  $entry.focus();
+}
 
 const isNum = value => {
   String(value).replace(/^\s+|\s+$/g, "");
@@ -9,32 +18,55 @@ const isNum = value => {
   return reg.test(value);
 }
 
+const isValidUnit = value => {
+  return (value >= 100) && (value % 100 === 0);
+}
+
 const isValid = value => {
   if ((value === "") || (value === null) || (value === undefined)) {
     alert('값을 입력하세요');
   } else if (!isNum(value)) {
     alert('숫자만 입력하세요');
-  } else if (isNum(value) && ((value < 100))) {
-    alert('2~9 사이의 숫자만 입력하세요');
+  } else if (!isValidUnit(value)) {
+    alert('100원, 1000원만 투입하세요');
   } else return true;
   refreshInput();
   return;
 }
 
-const refreshInput = () => {
-  $entry.value = "";
-  $entry.focus();
-}
-
 const handleEntry = e => {
   const { target: { value } } = e;
   if (isValid(value)) {
-    console.log(value);
+    paid += Number(value);
+    $paid.innerHTML = `${paid}원 입니다.`;
   }
   refreshInput();
   return;
 }
 
+const isPaid = price => {
+  if (paid < price) {
+    alert('잔액이 부족합니다.')
+  } else return true;
+  return;
+}
+
+const handleSelects = e => {
+  const { target: { dataset: { price, select } } } = e;
+  if (isPaid(price)) {
+
+    selectedArr.push(select);
+    if (selectedArr.length > 0) {
+      exithtml = selectedArr.map(select => select).join(",");
+      $exit.innerHTML = exithtml;
+    }
+
+    paid -= price;
+    $paid.innerHTML = paid;
+  }
+
+}
+
 $entryForm.addEventListener("submit", e => e.preventDefault());
 $entry.addEventListener("change", handleEntry);
-
+$selects.addEventListener("click", handleSelects);
