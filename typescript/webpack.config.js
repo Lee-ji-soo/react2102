fa34e8b3e865d6f8d1871cd8c66e3723158a4f5c
Lee-ji-soo <nbsp3@naver.com>
2021-03-21 01:15:17 +0900

    typescript setting config

---
+++ typescript/webpack.config.js
@@ -0,0 +1,42 @@
+const mode = process.env.NODE_ENV || "development";
+
+module.exports = {
+  mode,
+  entry: "./src/index.tsx",
+  output: {
+    path: __dirname + "/dist",
+    publicPath: "/",
+    filename: "[name].js"
+  },
+  resolve: {
+    extensions: [".ts", ".tsx", ".js"]
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env"]
+          }
+        }
+      },
+      {
+        test: /\.(ts|tsx)$/,
+        exclude: /node_modules/,
+        use: [
+          {
+            loader: "ts-loader"
+          }
+        ]
+      }
+    ]
+  },
+  devServer: {
+    contentBase: "./dist",
+    port: 3000,
+    historyApiFallback: true
+  }
+}
