761ebbd8d26d940a4c87242db75712cb13dcd9ae
이지수 <soolee@sooleeandmm.local>
2021-04-04 23:38:12 +0900

    typescript practice build  todolist in command line

--- typescript-todolist/src/index.ts
+++ typescript-todolist/src/index.ts
@@ -1,12 +1,13 @@
-import { Command, CommandPrintTodos } from "./Commands";
+import chalk from "chalk";
+import { Command, CommandDeleteTodos, CommandNewTodos, CommandPrintTodos } from "./Commands";
 import { waitForInput } from "./Input";
 import Todo from "./Todo";
-import { AppState, Priority } from "./type";
+import { Action, AppState, Priority } from "./type";
 
-const commands: Command[] = [new CommandPrintTodos()];
+const commands: Command[] = [new CommandPrintTodos(), new CommandNewTodos(), new CommandDeleteTodos()];
 
 async function main() {
-  const state: AppState = {
+  let state: AppState = {
     todos: [
       new Todo("test1", Priority.High),
       new Todo("test2", Priority.Medium),
@@ -15,17 +16,36 @@ async function main() {
   }
   while (true) {
     console.clear();
+    
     for (const command of commands) {
       console.log(command.toString());
     }
     console.log();
-    const key = await waitForInput("input command: ");
+    const key = await waitForInput(chalk`{yellow.bold input command:} `);
     console.clear();
     const command = commands.find(item => item.key === key);
     if (command) {
-      await command.run(state);
+      const action = await command.run(state);
+      if(action){
+        state = getNextState(state,action)
+      }
     }
   }
 };
 
 main();
+
+function getNextState(state:AppState, action:Action): AppState{
+  switch(action.type){
+    case "newTodo":
+      return{
+        ...state,
+        todos:[...state.todos, new Todo(action.title, action.priority)]
+      }
+   case "deleteTodo":
+     return{
+       ...state,
+       todos: state.todos.filter(item => item.id !== action.id)
+     }
+  }
+}
