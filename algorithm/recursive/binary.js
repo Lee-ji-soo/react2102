ec109f7da06a5fe3ac77eddbcf5488c53558d6e2
이지수 <soolee@sooleeandmm.local>
2021-04-16 23:47:01 +0900

    algorithm - recursive - 문자열뒤집기

--- algorithm/recursive/binary.js
+++ algorithm/recursive/binary.js
@@ -1,5 +1,5 @@
 
-// 11 을 2진법으로 바꿔라 binary change
+// Q1. 11 을 2진법으로 바꿔라 binary change
 // 2  11  1
 // 2   5  1
 // 2   2  0
@@ -25,17 +25,12 @@ while(true){
 console.log('binary-loop', result.split("").reverse().join(""));
 
 //재귀함수
-
 function binary(x){
-  if(x === 0 || x === 1){
-    console.log('current x:', x)
-    return String(x)
+  if(x === 1 || x === 0){
+    return x
   }
-  const current =  binary(Math.floor(x / 2)) + String(x % 2)
-  console.log('current x:', x)
-  console.log('curretn return: ',current);
-  return current
-}
+  return binary(Math.floor(x / 2)) + String(x % 2)
+};
 
 console.log('binary-recursive', binary(11));
 
@@ -43,3 +38,35 @@ console.log('binary-recursive', binary(11));
 // binary(5)    return  binary(2) + String(1)
 // binary(2)    return  binary(1) + String(0)
 // binary(1)    return  String(1)
+
+
+//Q2. 문자열 뒤집기
+
+//loop
+let resultText = "";
+let str = "soolee";
+while(true){
+  if(str.length === 1){
+    resultText += str ; 
+    // resultText = str + resultText;
+    break;
+  }
+  let strArr = str.split("");
+  resultText += String(strArr.pop());
+  // resultText = String(strArr.pop()) + resultText;
+  console.log('2', resultText)
+  str = strArr.join("");
+}
+console.log("reverse-text_recursive", resultText)
+
+
+//recursive
+let str2 = "soolee"
+function reverseText(str){
+  console.log(str)
+  if( str.length === 1){
+    return str
+  }
+  return String(str[str.length-1]) + reverseText(String(str.slice(0, str.length-1)))
+}
+console.log('reverseText', reverseText(str2))
