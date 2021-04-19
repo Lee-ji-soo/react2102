91a667b61050a3d5c4a3f25a99d8d09852a784b3
Lee-ji-soo <nbsp3@naver.com>
2021-04-20 01:03:38 +0900

    install python

---
+++ algorithm/pythonprac/1.py
@@ -0,0 +1,8 @@
+Python 3.9.4 (v3.9.4:1f2e3088f3, Apr  4 2021, 12:19:19) 
+[Clang 12.0.0 (clang-1200.0.32.29)] on darwin
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> x=3
+>>> y=7
+>>> print(x+y)
+10
+>>> 
