8c2736df476efde1fed8873e12a1f0882e2326ba
Lee-ji-soo <nbsp3@naver.com>
2021-04-19 23:41:05 +0900

    algorithm 선택정렬 with func

--- algorithm/array/selectsort.js
+++ algorithm/array/selectsort.js
@@ -2,7 +2,14 @@
 let a = [10, 11, 9, 2, 3, 6, 5, 4];
 let sortedArr = [];
 
-function min(a){
+
+const reducerMin = (acc,cur) => { return acc < cur ? acc : cur; }
+
+function min2(a){
+  return a.reduce(reducerMin);
+}
+
+function min1(a){
   let min = a[0]
   for( i of a ){
     if ( i < min ){
@@ -21,7 +28,7 @@ function findIndex(minNum){
 }
 
 for(let i = 0; i < 8; i++){
-  minNum = min(a);
+  minNum = min1(a);
   sortedArr.push(minNum);
   a.splice(findIndex(minNum), 1);
 }
