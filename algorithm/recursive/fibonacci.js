898a5027bd3a314eecd3f1d0f817add15bf47642
Lee-ji-soo <nbsp3@naver.com>
2021-04-23 23:42:42 +0900

    algorithm fibonacci 정리

--- algorithm/recursive/fibonacci.js
+++ algorithm/recursive/fibonacci.js
@@ -1,24 +1,21 @@
 // 1 1
+// loop
 let a = 1;
 let b = 1;
-for(let i=0; i<6; i++){
+for (let i = 0; i < 6; i++) {
   let c = a + b;
   a = b;
   b = c;
 }
-
 console.log(b);
 
 //recursive
-
-let add = 0 ;
-function fibonacci(x){
-  if(x === 1 || x === 2){
-    console.log("x===1 || x===2",x)
+let add = 0;
+function fibonacci(x) {
+  if (x === 1 || x === 2) {
     return 1;
   }
-  console.log("x", x);
-  return fibonacci( x-1 ) + fibonacci( x-2 );
+  return fibonacci(x - 1) + fibonacci(x - 2);
 }
 console.log('fibonacci', fibonacci(5));
 // 5 f(4) + f(3)
