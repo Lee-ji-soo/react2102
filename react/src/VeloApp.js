dd26f738af812ba5b6038702290f3978f4c3abe8
Lee-ji-soo <nbsp3@naver.com>
2021-07-06 20:23:39 +0900

    use useState

--- react/src/VeloApp.js
+++ react/src/VeloApp.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { BrowserRouter, Redirect, Route, Switch } from 'react-router-dom';
-import Home from "./veloact/Home";
-import Counter from "./veloact/Counter";
+import Home from './veloact/Home';
+import Counter from './veloact/Counter';
 
 const VeloApp = () => {
   return (
@@ -9,11 +9,12 @@ const VeloApp = () => {
       <BrowserRouter>
         <Switch>
           <Route exact path="/" component={Home} />
-          <Route exact path="/counter" component={Counter}/>
+          <Route exact path="/counter" component={Counter} />
           <Redirect path="/*" to="/" />
         </Switch>
       </BrowserRouter>
-    </>);
+    </>
+  );
 };
 
 export default VeloApp;
