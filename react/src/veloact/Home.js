3978af215fca199bb800470d930371f6bc620edb
Lee-ji-soo <nbsp3@naver.com>
2021-07-12 10:56:38 +0900

    react setusers

--- react/src/veloact/Home.js
+++ react/src/veloact/Home.js
@@ -1,32 +1,74 @@
-import React from 'react';
+import React, { useState, useRef } from 'react';
 import Hello from './Hello';
 import Wrapper from './Wrapper';
-import CreateUser from "./CreateUser";
-import UserList from "./UserList";
+import CreateUser from './CreateUser';
+import UserList from './UserList';
+
+export const usersSample = [
+  {
+    id: 1,
+    username: 'velopert',
+    email: 'public.velopert@gmail.com',
+  },
+  {
+    id: 2,
+    username: 'tester',
+    email: 'tester@example.com',
+  },
+  {
+    id: 3,
+    username: 'liz',
+    email: 'liz@example.com',
+  },
+];
 
 function Home() {
-  const users = [
-    {
-      id: 1,
-      username: 'velopert',
-      email: 'public.velopert@gmail.com',
-    },
-    {
-      id: 2,
-      username: 'tester',
-      email: 'tester@example.com',
-    },
-    {
-      id: 3,
-      username: 'liz',
-      email: 'liz@example.com',
-    },
-  ];
+  const [inputs, setInputs] = useState({
+    username: '',
+    email: '',
+  });
+
+  const [users, setUsers] = useState(usersSample);
+
+  const { username, email } = inputs;
+
+  const onChange = (e) => {
+    const { name, value } = e.target;
+    setInputs({
+      ...inputs,
+      [name]: value,
+    });
+  };
+
+  const nextId = useRef(4);
+
+  const onCreate = () => {
+
+    console.log(nextId.current);
+
+    const user = {
+      id: nextId.current,
+      username,
+      email,
+    };
+    setUsers([...users, user]);
+
+    setInputs({
+      username: '',
+      email: '',
+    });
+    nextId.current += 1;
+  };
 
   return (
     <Wrapper>
-      <CreateUser />
-      <UserList users={users}/>
+      <CreateUser
+        email={email}
+        username={username}
+        onChange={onChange}
+        onCreate={onCreate}
+      />
+      <UserList users={users} />
     </Wrapper>
   );
 }
